f25dcf291314310635d91b1d752720b758eb7b2c
maduuk <161736775+maduuk@users.noreply.github.com>
2024-10-17 08:28:49 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,12 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantaia, aventura
+// guardios da galaxia, 12, fantasia, aventura
+// as aventurs de pi, 10, drama, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+// ladroes de bicicleta, 12, drama
+// o menino que decobriu o vento, 14, drama
+
 function setup() {
   createCanvas(400, 400);
 }
